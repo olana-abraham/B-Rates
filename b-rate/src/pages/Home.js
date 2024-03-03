d243619ebbf34bf5bc008937e9519fa7d51f1ab1
dude2485 <49ersfandude1000@gmail.com>
2024-03-03 05:07:27 -0800

    Finished reformatting Home page and Reviews, and linked web pages.

--- b-rate/src/pages/Home.js
+++ b-rate/src/pages/Home.js
@@ -1,3 +1,12 @@
+import ImageSlider from '../ImageSlider';
+import '../home.css';
+
+
 export default function Home() {
-    return <h1>Home</h1>
+    return (
+        <div className='slider'>
+            <ImageSlider />
+        </div>
+    )
+
 }
