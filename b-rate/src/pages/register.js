cdf47d3373502e8ad939c55c90c138f4e96d5dc7
dude2485 <49ersfandude1000@gmail.com>
2024-03-03 18:49:52 -0800

    Linked register to database to save new users.

--- b-rate/src/pages/register.js
+++ b-rate/src/pages/register.js
@@ -1,56 +1,54 @@
 import React from 'react';
 import './Register.css';
-import {useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
-import supabase from "../config/supabaseClient"
+import supabase from "../config/supabaseClient.js"
 
 const Register = () => {
     const [email, setEmail] = useState('')
-  const [username, setUsername] = useState('')
-  const [pass1, setPass1] = useState('')
-  const [pass2, setPass2] = useState('')
+    const [username, setUsername] = useState('')
+    const [pass1, setPass1] = useState('')
+    const [pass2, setPass2] = useState('')
 
 
-async function handleSubmit(e){
-    e.preventDefault()
-    try{
-    // if (pass1 != pass2) {
-    //     throw new Error("Passwords do not matcSh");
-    // }
-        const { data, error } = await supabase.auth.signUp({
-        email: "ndamtew10@g.ucla.edu",
-        password: "typeshit",
-        options: {
-            data: { username: "natiye", }
-     }
-    })
-    if(error) throw error
-      alert("Check your email for verification link")
-    } 
-    catch(error)
-    {
-    alert(error)
+    async function handleSubmit(e) {
+        e.preventDefault()
+        try {
+            if (pass1 != pass2) {
+                throw new Error("Passwords do not match");
+            }
+            let { data, error } = await supabase.auth.signUp({
+                email: email,
+                password: pass1,
+                options: {
+                    data: { username: username }
+                }
+            })
+            if (error) throw error
+            alert("Check your email for verification link")
+        }
+        catch (error) {
+            alert(error)
+        }
     }
-    }
 
-    return(
+    return (
         <div className='wrapper'>
             <form onSubmit={handleSubmit}>
                 <h1>Register</h1>
                 <div className="input-box">
-                    <input type="text" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)}required />
+                    <input type="text" placeholder='Username' onChange={(e) => setUsername(e.target.value)} required />
                 </div>
                 <div className="input-box">
-                    <input type="text" placeholder='Email'  value={email} 
-                        onChange={(e) => setEmail(e.target.value)}required/>
-                        console.log(e.target.value)
+                    <input type="text" placeholder='Email'
+                        onChange={(e) => setEmail(e.target.value)} required />
                 </div>
                 <div className="input-box">
-                    <input type="password" placeholder='Create Password' value={pass1} 
+                    <input type="password" placeholder='Create Password' 
                         onChange={(e) => setPass1(e.target.value)} required />
                 </div>
                 <div className="input-box">
-                    <input type="password" placeholder='Confirm Password'  value={pass2} 
+                    <input type="password" placeholder='Confirm Password'
                         onChange={(e) => setPass2(e.target.value)} required />
                 </div>
                 <button type="submit" >Register</button>
