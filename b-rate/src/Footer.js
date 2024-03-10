22f8936f67db01ef663cba185e1ef73682715f15
olana-abraham <olana.abraham@hgs.hiddengeniusproject.org>
2024-03-09 17:39:06 -0800

    Completed Footer

--- b-rate/src/Footer.js
+++ b-rate/src/Footer.js
@@ -9,25 +9,27 @@ const Footer = () => {
                     <div className="col">
                         <h4>About Us</h4>
                         <ul className = "list-unstyled">
-                            <li>343-024-2344</li>
-                            <li>Moscow, Russia</li>
+                            <li>brate042@gmail.com</li>
+                            <li>Los Angeles, CA</li>
                             <li>330 De Neve Drive</li>
                         </ul>
                     </div>
                     <div className="col">
-                        <h4>About Us</h4>
+                        <h4>Help Center</h4>
                         <ul className = "list-unstyled">
-                            <li>343-024-2344</li>
-                            <li>Moscow, Russia</li>
-                            <li>330 De Neve Drive</li>
+                            <li><a className='test' href="/">Home</a></li>
+                            <li><a className='test' href="./Login">Login</a></li>
+                            <li><a className='test' href="./register">Register</a></li>
+                            <li><a className='test' href="./reviews">Reviews</a></li>
+                            <li><a className='test' href="https://menu.dining.ucla.edu/hours/">Menu</a></li>
                         </ul>
                     </div>
-                    <div className="col">
-                        <h4>About Us</h4>
-                        <ul className = "list-unstyled">
-                            <li>343-024-2344</li>
-                            <li>Moscow, Russia</li>
-                            <li>330 De Neve Drive</li>
+                    <div class="col-md-4">
+                        <h4>Connect with Us</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="https://www.tiktok.com/@ucla?lang=en" className='test'>Tiktok</a></li>
+                            <li><a href="https://twitter.com/AbrahamOlana" className='test'> Twitter</a></li>
+                            <li><a href="https://www.instagram.com/nattiicho/" className='test'>Instagram</a></li>
                         </ul>
                     </div>
                 </div>
