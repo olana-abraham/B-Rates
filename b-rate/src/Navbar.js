6ad5ea2f68d29cfb3dfd3c8dd6f15aff36d9178f
deannali <deanali2003@gmail.com>
2024-03-10 22:33:32 -0700

    added a collpasing and expanding search bar

--- b-rate/src/Navbar.js
+++ b-rate/src/Navbar.js
@@ -2,6 +2,7 @@ import { Link, useMatch, useResolvedPath, useLocation } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaSearch } from "react-icons/fa";
 import logo from '/Users/deanali/Desktop/CS/CS35L/CS35L Project/B-Rates-3/b-rate/src/b-rate-logo.png';
+import { useEffect } from "react";
 
 
 
@@ -12,6 +13,14 @@ export default function Navbar() {
     const isLoginPage = location.pathname === "/Login";
     const isRegisterPage = location.pathname === "/register";
     const isForgotPage = location.pathname == "/forgot";
+
+     useEffect(() => {
+        const searchBox = document.getElementById("search-bar");
+        const icon = document.getElementsByClassName("submit")[0];
+        icon.onclick=function(){
+            searchBox.classList.toggle("active");
+        }
+    }, []); 
 
     return (
         <nav className="nav">
@@ -28,9 +37,22 @@ export default function Navbar() {
                 
             </ul>
             )}
+            <body>
+            <div className = "search" id="search-bar" >
+                <input class="search-txt" type="text" placeholder="Search"/>
+                <button class="submit"><FaSearch /></button>
+            </div>
+            
                 
-                <Link to="/" className="search"><FaSearch /></Link>
+               
 
+           
+
+
+       
+            </body>
+
+            
         </nav>
     )
 }
@@ -44,5 +66,13 @@ function CustomLink({ to, children, ...props }) {
                 {children}
             </Link>
         </li>
+         
+               
+
     )
+
+ 
+
+
+ 
 }
