bef3e33ca1a76332bc5b562e45c44697d1f730f8
Tre-Bradshaw <tre.bradshaw@icloud.com>
2024-03-10 23:41:56 -0700

    Create profile page

--- b-rate/src/pages/Profile.js
+++ b-rate/src/pages/Profile.js
@@ -7,17 +7,23 @@ export default function Profile(){
     return(
         <div> 
             <Navbar />
-            <div className='profile-wrapper'>
-                
-                <div className="profile-info">
-                    <h1>First and Last Name</h1>
-                    <h2>Graduating in _ _ _ _</h2>
-                </div>
-                <div className="profile-about">
-                    <h1>Bio:</h1>
-                </div>
-
+            <div className='name-year'> 
+                <h1>FirstName LastName</h1>
+                <h2>Graduating 20**</h2>
             </div>
+                <body className='profile-body'>
+                    <div className="profile-container">
+                        <div className='profile-header'>
+                            <h2>About</h2>
+                            <h3>Favorite Dining Hall</h3>
+                            <h4>De Neve</h4>
+
+                        </div>
+                        <div className='about'>
+                            <p className='about-body'> Hi my name is _____. I am a _____ student and I really enjoy eating meals from _____.</p>
+                        </div>
+                    </div>
+                </body>
         </div>
     )
 }
