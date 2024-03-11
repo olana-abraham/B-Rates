25ad01dd5058e64dcb950beae3eb6e1ddcf9d1d9
olana-abraham <olana.abraham@hgs.hiddengeniusproject.org>
2024-03-10 22:49:57 -0700

    Added sign out feature and profile if logged in

--- b-rate/src/pages/Home.js
+++ b-rate/src/pages/Home.js
@@ -1,12 +1,21 @@
 import ImageSlider from '../ImageSlider';
 import '../home.css';
 import Navbar from "../Navbar";
-import homeImage from '/Users/deanali/Desktop/CS/CS35L/CS35L Project/B-Rates-3/b-rate/src/inforgraphicb-rates.png';
+import homeImage from '../inforgraphicb-rates.png';
+import React, { useState, useEffect } from 'react';
+import supabase from "../config/supabaseClient"
 
 
 
 
 export default function Home() {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+      const session = supabase.auth.getSession();
+      setUser(session?.user ?? null);
+    }, []);
+
     return (
         <div>
         <Navbar />
@@ -29,8 +38,13 @@ export default function Home() {
             <h1>Welcome To B-Rates</h1>
             <h2>Explore campus dining like never before with our community-driven ratings!</h2>
            <p>"Welcome to B-Rate, your ultimate destination for exploring and reviewing the diverse dining options available at UCLA! Whether you're a food enthusiast, a health-conscious eater, or simply looking for the best dining hall experience, we've got you covered."</p>
-          <a href="./Reviews"><button className = "homebutton1">Read Reviews</button></a> 
-            <a href="./Login"><button className = "homebutton2">Login</button></a> 
+          <a href="./Reviews"><button className = "homebutton1">Read Reviews</button></a>
+            {!user &&(
+                <a href="./Login"><button className = "homebutton2">Login</button></a> 
+            )}
+            {user &&(
+                <a href="./"><button className = "homebutton2">Profile</button></a> 
+            )}
             </div>
 
 
