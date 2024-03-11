a08ead45a1312e9e698b620f1a126560acd13623
olana-abraham <olana.abraham@hgs.hiddengeniusproject.org>
2024-03-10 22:49:57 -0700

    Added sign out feature and profile if logged in

--- b-rate/src/Navbar.js
+++ b-rate/src/Navbar.js
@@ -1,9 +1,9 @@
-import { Link, useMatch, useResolvedPath, useLocation } from "react-router-dom";
+import { Link, useMatch, useResolvedPath, useLocation} from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaSearch } from "react-icons/fa";
-import logo from '/Users/deanali/Desktop/CS/CS35L/CS35L Project/B-Rates-3/b-rate/src/b-rate-logo.png';
-import { useEffect } from "react";
-
+import logo from './b-rate-logo.png';
+import React, { useState, useEffect } from 'react';
+import supabase from "./config/supabaseClient"
 
 
 export default function Navbar() {
@@ -12,7 +12,22 @@ export default function Navbar() {
     // Check if the current location matches the login or register page routes
     const isLoginPage = location.pathname === "/Login";
     const isRegisterPage = location.pathname === "/register";
-    const isForgotPage = location.pathname == "/forgot";
+    const isForgotPage = location.pathname === "/forgot";
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+      const session = supabase.auth.getSession();
+      setUser(session?.user ?? null);
+    }, []);
+
+    const handleSignOut = async () => {
+        try {
+          await supabase.auth.signOut();
+        } catch (error) {
+          console.error('Error signing out:', error.message);
+        }
+      };
 
      useEffect(() => {
         const searchBox = document.getElementById("search-bar");
@@ -26,30 +41,29 @@ export default function Navbar() {
         <nav className="nav">
             <Link to="/" className="site-title"><GiHamburgerMenu /></Link>
 
-            {!isLoginPage && !isRegisterPage && !isForgotPage &&(
-            <ul className = "mainlinks">
-                
-                <CustomLink to="/" className="home">Home </CustomLink>
-                <CustomLink to="/Reviews" className="reviews"> Reviews </CustomLink>
-                <CustomLink to="/" className="logo"><img src={logo} alt="homelogo" className='logo' /></CustomLink>
-                <CustomLink to="/Login" className="login">Login</CustomLink>
-                <CustomLink to="/register" className="register">Register</CustomLink>
-                
-            </ul>
+            {!isLoginPage && !isRegisterPage && !isForgotPage && !user && (
+                    <ul className = "mainlinks">
+                        <CustomLink to="/" className="home">Home </CustomLink>
+                        <CustomLink to="/Reviews" className="reviews"> Reviews </CustomLink>
+                        <CustomLink to="/" className="logo"><img src={logo} alt="homelogo" className='logo' /></CustomLink>
+                        <CustomLink to="/Login" className="login">Login</CustomLink>
+                        <CustomLink to="/register" className="register">Register</CustomLink>
+                    </ul>
+            )}
+            {user && (
+                <ul className = "mainlinks">
+                    <CustomLink to="/" className="home">Home </CustomLink>
+                    <CustomLink to="/Reviews" className="reviews"> Reviews </CustomLink>
+                    <CustomLink to="/" className="logo"><img src={logo} alt="homelogo" className='logo' /></CustomLink>
+                    <CustomLink to="/" className="profile">Profile</CustomLink>
+                    <CustomLink onClick={handleSignOut} className="register">Sign Out</CustomLink>
+                </ul>                
             )}
             <body>
             <div className = "search" id="search-bar" >
                 <input class="search-txt" type="text" placeholder="Search"/>
                 <button class="submit"><FaSearch /></button>
             </div>
-            
-                
-               
-
-           
-
-
-       
             </body>
 
             
