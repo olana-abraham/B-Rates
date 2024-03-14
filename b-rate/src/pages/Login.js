3de1fff0464c767a0d173c5aeb2ad434df8f7a1c
deannali <deanali2003@gmail.com>
2024-03-13 20:42:27 -0700

    linked the forgot password page to link

--- b-rate/src/pages/Login.js
+++ b-rate/src/pages/Login.js
@@ -49,12 +49,14 @@ const Login = () => {
 
                 <div className="remember-forgot">
                     <label><input type="checkbox" />Remember Me </label>
-                    <a href="">Forgot Password?</a>
+                    <Link to="/forgot" > Forgot Password?</Link>
+
                 </div>
 
                 <button type="submit">Sign In</button>
                 <div className="register-link">
                     <p>Don't have an account? <a href="./register">Register</a></p>
+
 
                 </div>
 
